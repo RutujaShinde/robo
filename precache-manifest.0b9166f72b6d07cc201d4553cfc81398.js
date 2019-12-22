3b8a5d3808ab946bd97d0c07204c9cc91766895e
Rutuja Shinde <rutuja.shinde56@gmail.com>
2019-12-22 13:14:02 +0530

    Updates

---
+++ precache-manifest.0b9166f72b6d07cc201d4553cfc81398.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a22622692ecbc3a50b976c337626537",
+    "url": "/robo/index.html"
+  },
+  {
+    "revision": "dc46e85411963e31a321",
+    "url": "/robo/static/css/main.d8f659c3.chunk.css"
+  },
+  {
+    "revision": "18698dd5988d654bf99a",
+    "url": "/robo/static/js/2.b305ee2f.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/robo/static/js/2.b305ee2f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "dc46e85411963e31a321",
+    "url": "/robo/static/js/main.704bf559.chunk.js"
+  },
+  {
+    "revision": "7b6f9fe95a3b06740f13",
+    "url": "/robo/static/js/runtime-main.8f208648.js"
+  }
+]);
